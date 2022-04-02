db457772157b413202f2609d7448ec91573107aa
Rus Galeev <spyder44@yandex.ru>
2022-04-02 10:09:01 +0300

    main.kt fun countCommission min commission added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 var commissionRate: Double = 0.0075
 fun main() {
-    val amount = 66793 //сумма перевода в копейках
+    val amount = 667 //сумма перевода в копейках
     var commission = countCommission(amount)
     println("Комиссия составит $commission рублей")
 }
 
 private fun countCommission(amount: Int): Int {
-    return (commissionRate * amount).toInt()
+    return if ((commissionRate * amount).toInt() < 35) 35 else (commissionRate * amount).toInt()
 }
